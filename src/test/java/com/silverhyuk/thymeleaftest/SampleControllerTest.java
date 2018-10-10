eb3a18aa78fd3fbe4281ed127d3166476034d9ac
silverhyuk84 <silverhyuk84@gmail.com>
2018-10-10 21:53:29 +0900

    Initial commit

---
+++ src/test/java/com/silverhyuk/thymeleaftest/SampleControllerTest.java
@@ -0,0 +1,33 @@
+package com.silverhyuk.thymeleaftest;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", Matchers.is("eunhyuk")))
+                .andExpect(content().string(containsString("eunhyuk")))
+                .andDo(print());
+    }
+}
